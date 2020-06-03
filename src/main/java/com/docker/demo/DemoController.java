c0af2bb5ea3074ec9aee3825d33a20094982c452
Uttam Barik <uttamkb@gmail.com>
2020-06-03 12:02:53 +0530

    1st commit

---
+++ src/main/java/com/docker/demo/DemoController.java
@@ -0,0 +1,13 @@
+package com.docker.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @GetMapping(path="/")
+    public String runImage(){
+        return  "docker image created and running";
+
+    }
+}
